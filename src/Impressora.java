a7211d3c850ad7abc00fc1f43d126806f584bc65
Victor Lira <vl@cin.ufpe.br>
2018-11-09 16:58:58 -0300

    fixes #3

--- src/Impressora.java
+++ src/Impressora.java
@@ -2,6 +2,9 @@
 public class Impressora {
 
 	public static void imprimir(String texto) {
+		if (texto == null) {
+			throw new IllegalArgumentException("texto == null");
+		}
 		System.out.println(texto);
 	}
 }
